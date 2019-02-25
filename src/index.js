cdeca3e2e76e218e811cdc39aae308377ff838c3
Angel Jimenez <ajimenez@nayarsystems.com>
2019-02-25 15:56:21 +0100

    Factory function that detects environment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const wsweb = require("./wsweb.js");
 const wswechat = require("./wswechat.js");
 
-exports.WsWeb = wsweb.WsWeb;
-exports.WsWeChat = wswechat.WsWeChat;
+exports.create = () => {
+  if (window && window.WebSocket) {
+    console.log("multiws: detected web/Cordova");
+    return new wsweb.WsWeb();
+  }
+  if (global && global.wx) {
+    console.log("multiws: detected WeChat");
+    return new wswechat.WsWeChat();
+  }
+  throw new Error("Unknown environment");
+};
